ac537eb7b78c30d00f6678d162a3e35cf6b0610f
drafear <drafear@outlook.com>
2019-03-06 19:00:59 +0900

    rle

--- lib/rle.cpp
+++ lib/rle.cpp
@@ -1,19 +1,19 @@
 // ランレングス圧縮
 vector<pair<char, int>> rle(string s) {
-    char prev = '\0';
-    int cnt = 0;
-    vector<pair<char, int>> res;
-    REP(i, s.size()+1) {
-        if (i == s.size() || s[i] != prev) {
-            if (prev != '\0') {
-                res.pb({prev, cnt});
-            }
-            prev = s[i];
-            cnt = 1;
-        }
-        else {
-            ++cnt;
-        }
+  char prev = '\0';
+  int cnt = 0;
+  vector<pair<char, int>> res;
+  REP(i, s.size()+1) {
+    if (i == s.size() || s[i] != prev) {
+      if (prev != '\0') {
+        res.pb({prev, cnt});
+      }
+      prev = s[i];
+      cnt = 1;
     }
-    return res;
+    else {
+      ++cnt;
+    }
+  }
+  return res;
 }
